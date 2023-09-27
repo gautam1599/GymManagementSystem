76c9a0e3ab74af568b5075be67bd0a750b92d012
GautamPradyumna <46934583+GautamPradyumna@users.noreply.github.com>
2023-09-27 15:39:25 -0700

    Update index.js

--- gcvi_backend/gcvi_backend/index.js
+++ gcvi_backend/gcvi_backend/index.js
@@ -5,7 +5,7 @@ const cors=require('cors');
 const app=express();
 app.use(cors(
     {
-        origin:[],
+        origin:["https://gym-management-backend.vercel.app"],
         methods:["POST","GET"],
         credentials:true
     }
